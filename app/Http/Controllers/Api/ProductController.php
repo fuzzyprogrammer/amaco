699a735201f63202cabc9f9f74774bdd8dce2873
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2020-11-26 15:20:24 +0530

    added the delete api link

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,26 +5,16 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\Role;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $products = Product::all();
         return ($products);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+//
     public function store(Request $request)
     {
         $rules = [
@@ -37,41 +27,34 @@ class ProductController extends Controller
             'mrp' => 'required',
             'real_price' => 'required',
         ];
+        
         $validatedData = $request->validate($rules);
-        if($validatedData){
-            $product = new Product;
-            $product->category_id = $request->category_id;
-            $product->division_id = $request->division_id;
-            $product->name = $request->name;
-            $product->description = $request->description;
-            $product->unit_of_measure = $request->unit_of_measure;
-            $product->unit_price = $request->unit_price;
-            $product->mrp = $request->mrp;
-            $product->real_price = $request->real_price;
-            $product->save();
-            return(
-                $product
+
+        if($validatedData->fails()){
+            $returnData = array(
+                'status'=>'error',
+                'message'=>'Please review fields',
+                'error'=>$validatedData->errors()->all()
             );
+            return response()->json($returnData, 500);
         }
-        else{
-            return ['error'=>'complete all the fields'];
-        }
+
+        $product = new Product;
+        $product->category_id = $request->category_id;
+        $product->division_id = $request->division_id;
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->unit_of_measure = $request->unit_of_measure;
+        $product->unit_price = $request->unit_price;
+        $product->mrp = $request->mrp;
+        $product->real_price = $request->real_price;
+        $product->save();
+        return($product);
+//
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+//
     public function show($id)
     {
         $product = Product::findOrfail($id);
@@ -91,12 +74,7 @@ class ProductController extends Controller
         );
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+//
     public function update(Request $request, $id)
     {
         // $rules = [
@@ -114,26 +92,14 @@ class ProductController extends Controller
         $product = Product::findOrfail($id);
         $product->update($request->all());
         return ($product);
-
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    //
     public function destroy($id)
     {
-        //
+        $product=Product::findOrfail($id);
+        $res = $product->delete();
+        if($res){
+            return (['msg'=>$product->name.' is successfully deleted']);
+        }
     }
 }
