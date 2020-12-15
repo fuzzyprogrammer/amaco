d08c99d44d87233b3fb86973aa4729fe286671c1
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2020-12-15 12:01:03 +0530

    updated

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -102,7 +102,7 @@ class RFQController extends Controller
 
             foreach ($data['rfq_details'] as $rfq_detail) {
                 $_rfq_detail = RFQDetails::create([
-                    'product_id' => $rfq_detail->id,
+                    // 'product_id' => $rfq_detail->id,
                     'description' => $rfq_detail->descriptionss,
                     'quantity_required' => $rfq_detail->quantity,
                     // 'rfq_id' => $this->_rfq_id,
