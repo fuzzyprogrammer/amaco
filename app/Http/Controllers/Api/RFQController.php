29ddebe2ede9df10a1a5002f916c3af92dbbe18c
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2020-12-15 11:32:37 +0530

    updated

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -107,9 +107,9 @@ class RFQController extends Controller
             // dd($data['rfq_details']);
             // return $data['rfq_details'];
             foreach ($data['rfq_details'] as $rfq_detail) {
-                return 'inside foreach';
                 $_rfq_detail = new RFQDetails;
                 $_rfq_detail->product_id = $rfq_detail['id'];
+                dd($rfq_detail);
                 $_rfq_detail->rfq_id = $_rfq['id'];
                 $_rfq_detail->description = $rfq_detail['descriptionss'];
                 $_rfq_detail->quantity_required = $rfq_detail['quantity'];
