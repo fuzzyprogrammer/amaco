81cda9346b199ba146de5a2b18ffe6e461e81811
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2020-12-15 12:11:07 +0530

    updated

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -99,9 +99,9 @@ class RFQController extends Controller
             ]);
 
             $_rfq_id = $rfq['id'];
-            return $_rfq_id;
 
             foreach ($data['rfq_details'] as $rfq_detail) {
+                global $_rfq_id;
                 $_rfq_detail = RFQDetails::create([
                     'product_id' => $rfq_detail['id'],
                     'description' => $rfq_detail['descriptionss'],
