ba2ce188cddfd5eea6deff9fa47ff48290f91ecb
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-01-08 19:18:22 +0530

    updated

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Contact;
 use Illuminate\Http\Request;
-use Stichoza\GoogleTranslate\GoogleTranslate;
 
 
 class ContactController extends Controller
--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
-use Stichoza\GoogleTranslate\GoogleTranslate;
+// use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class ProductController extends Controller
 {
