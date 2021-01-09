9d163efc78fd5c0d62ddbf6a4b3813e148920c33
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-01-09 18:20:22 +0530

    updated

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -105,7 +105,7 @@ class InvoiceController extends Controller
         return [
             $invoice,
             $invoice->quotation->party,
-            $invoice->invoiceDetail->quotatioinDetail,
+            $invoice->invoiceDetail->quotationDetail,
             // $invoice->quotation,
             // $invoice->quotation->quotationDetail,
         ];
