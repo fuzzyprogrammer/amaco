9be3c83f4e1e23a46f9ee8b2aa280f372ac6a503
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-01-09 17:16:21 +0530

    updated

---
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getCurrentYear()
+    {
+        return substr(date('Y'), 2);
+    }
+
+    public function getLastInvoiceNo()
+    {
+        $invoice = Invoice::latest('created_at')->first();
+        if ($invoice) {
+            $latest_invoice_no = $invoice->invoice_no ? $invoice->invoice_no : 0;
+            return ($latest_invoice_no);
+        } else {
+            return ('AMINV-' . $this->getCurrentYear() . '-' . sprintf("%04d", 0));
+        }
+    }
+
+    public function getInvoiceNo()
+    {
+        $latest_invoice_no = $this->getLastInvoiceNo();
+        $last_year = substr($latest_invoice_no, 6, 2);
+        $current_year = $this->getCurrentYear();
+        // dd([$last_year, $current_year]);
+        if ($current_year != $last_year) {
+            return ('AMINV-' . $current_year . '-' . sprintf("%04d", 1));
+        } else {
+            return ('AMINV-' . $current_year . '-' . sprintf("%04d", ((int)substr($this->getLastInvoiceNo(), 9)) + 1));
+        }
+    }
+    public function index()
+    {
+        $invoices = Invoice::all();
+        return $invoices;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // $data = $request->json()->all();
+        // dd($data);
+        // dd($request->vat_in_value);
+        // dd($request->vat_in_value);
+        $data['invoice_no'] = $this->getInvoiceNo();
+        $data['issue_date'] = now();
+        $data['status'] = "New";
+        $data['quotation_id'] = $request['quotation_id'];
+        $data['total_value'] = $request['total_value'];
+        $data['discount_in_percentage'] = $request['discount_in_percentage'];
+        $data['vat_in_value'] = $request['vat_in_value'];
+        $data['grand_total'] = $request['grand_total'];
+        $invoice = Invoice::create([
+            'invoice_no' => $data['invoice_no'],
+            'issue_date' => $data['issue_date'],
+            'status' => $data['status'],
+            'quotation_id' => $data['quotation_id'],
+            'total_value' => $data['total_value'],
+            'discount_in_percentage' => $data['discount_in_percentage'],
+            'vat_in_value' => $data['vat_in_value'],
+            'grand_total' => $data['grand_total'],
+        ]);
+        return response()->json($invoice);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Invoice $invoice)
+    {
+        return [
+            $invoice,
+            $invoice->quotation,
+            // $invoice->quotation->quotationDetail,
+        ];
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Invoice $invoice)
+    {
+        $data = $request->all();
+        $data['status'] = 'Delivered';
+        $invoice->update($data);
+        return $invoice;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invoice $invoice)
+    {
+        return ($invoice->delete());
+    }
+}
