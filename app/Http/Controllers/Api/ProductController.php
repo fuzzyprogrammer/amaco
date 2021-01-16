87ea559ddab9a6310159b390f8b8e614c9d8a54e
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-01-16 14:13:09 +0530

    updated

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -82,7 +82,7 @@ class ProductController extends Controller
             ->where('products.id','=',$product)
             ->get();
 
-        $data = [$product, $productPrice->productPrice()];
+        $data = [$product, 'prices' => $productPrice->productPrice()];
         return response()->json($data);
 
     }
