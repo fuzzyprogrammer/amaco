04f22810b4ec13a54e4ad9c36accdaa0b55e31b2
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-01-16 15:54:33 +0530

    updated

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -75,7 +75,7 @@ class ProductController extends Controller
     {
         $productPrice = Product::where('id','=',$product)->first();
         $prices = $productPrice->productPrice->map(function ($productdetail){
-                return $productdetail->party;
+                return [$productdetail, $productdetail->party];
             });
         $product = DB::table('products')
             ->leftJoin('categories', 'categories.id', '=', 'products.category_id')
