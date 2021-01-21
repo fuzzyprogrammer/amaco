bbebea569e711bba535c4345ea1494b6190a8edb
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-01-21 11:17:59 +0530

    updated

--- app/Http/Controllers/Api/PartyController.php
+++ app/Http/Controllers/Api/PartyController.php
@@ -79,7 +79,7 @@ class PartyController extends Controller
         ]);
 
         if($party->party_code == null){
-            $party->update(['party_code'=>'c-'.sprintf('%05d', $party->id)]);
+            $party->update(['party_code'=>'C-'.sprintf('%05d', $party->id)]);
         }
 
         return response()->json([$party, $contact], 200);
