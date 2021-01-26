c2933595213bd3fb2b9c09357b606a567804fb77
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-01-26 18:17:16 +0530

    updated

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -60,7 +60,7 @@ class ExpenseController extends Controller
      */
     public function update(Request $request, Expense $expense)
     {
-        $request->is_paid = true;
+        $request->is_paid = 1;
         $expense->update($request->all());
         return response()->json($expense);
     }
