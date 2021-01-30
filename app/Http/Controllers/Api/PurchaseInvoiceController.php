575a33bf065562ff8970e3646f861296421b138c
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-01-30 16:43:09 +0530

    updated

--- app/Http/Controllers/Api/PurchaseInvoiceController.php
+++ app/Http/Controllers/Api/PurchaseInvoiceController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\PurchaseInvoice;
 use App\Models\PurchaseInvoiceDetail;
 use Illuminate\Http\Request;
