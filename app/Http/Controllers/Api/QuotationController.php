a02003b3e7f6f4216c18025e1c2e98d0e51d4c29
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-01-30 18:14:45 +0530

    updated

--- app/Http/Controllers/Api/QuotationController.php
+++ app/Http/Controllers/Api/QuotationController.php
@@ -298,16 +298,16 @@ class QuotationController extends Controller
             foreach ($data['quotation_details'] as $quotation_detail) {
                 $quotation_detail=QuotationDetail::where('id',$quotation_detail['id'])->first();
                 $quotation_detail->update([
-                    'quotation_id' => $quotation->id,
-                    'total_amount' => $quotation_detail->total_amount,
+                    // 'quotation_id' => $quotation->id,
+                    'total_amount' => $quotation_detail['total_amount'],
                     'analyse_id' => null,
-                    'product_id' => $quotation_detail->product_id,
-                    'purchase_price' => $quotation_detail->purchase_price,
-                    'description' => $quotation_detail->description,
-                    'quantity' => $quotation_detail->quantity,
-                    'margin' => $quotation_detail->margin,
-                    'sell_price' => $quotation_detail->sell_price,
-                    'remark' => $quotation_detail->remark,
+                    'product_id' => $quotation_detail['product_id'],
+                    'purchase_price' => $quotation_detail['purchase_price'],
+                    'description' => $quotation_detail['description'],
+                    'quantity' => $quotation_detail['quantity'],
+                    'margin' => $quotation_detail['margin'],
+                    'sell_price' => $quotation_detail['sell_price'],
+                    'remark' => $quotation_detail['remark'],
                 ]);
             }
             return response()->json(['msg' => 'successfully added']);
