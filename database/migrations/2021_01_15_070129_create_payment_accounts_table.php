799acba9686d964f7f216d329838e55909121e74
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-02-05 17:42:13 +0530

    updated

--- database/migrations/2021_01_15_070129_create_payment_accounts_table.php
+++ database/migrations/2021_01_15_070129_create_payment_accounts_table.php
@@ -15,7 +15,7 @@ class CreatePaymentAccountsTable extends Migration
     {
         Schema::create('payment_accounts', function (Blueprint $table) {
             $table->id();
-            $table->sting('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->make([
+            'email' => 'admin@email.com',
+            'name' => 'admin',
+            // 'email_verified_at' => now(),
+            'password' => bcrypt("admin@123"),
+        ]);
     }
 }
