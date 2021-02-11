38c081e393173292e4bea403e3052758c7ad6528
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-02-11 18:13:12 +0530

    updated

--- database/migrations/2020_11_26_041120_create_roles_table.php
+++ database/migrations/2020_11_26_041120_create_roles_table.php
@@ -15,8 +15,8 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->unsignedBigInteger('department_id');
+            $table->string('name');
+            // $table->unsignedBigInteger('department_id');
             $table->timestamps();
         });
     }
