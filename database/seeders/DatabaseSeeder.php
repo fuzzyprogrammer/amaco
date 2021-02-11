8e526f98ea12f5944f173e3b56caec6a8c7a7641
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-02-11 18:08:18 +0530

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::create([
-            'fname' => 'admin',
+        \App\Models\User::factory()->create([
+            'name' => 'superadmin',
+            'email' => 'superadmin@email.com',
+            'password' => bcrypt("superadmin@123"),
+            'role_id' => 1,
+            'jamath_id' => 2,
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
             'email' => 'admin@email.com',
-            'password' => bcrypt("admin@123")
+            'password' => bcrypt("admin@123"),
+            'role_id' => 2,
+            'jamath_id' => 2,
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'editor',
+            'email' => 'editor@email.com',
+            'password' => bcrypt("editor@123"),
+            'role_id' => 3,
+            'jamath_id' => 1,
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'guest',
+            'email' => 'guest@email.com',
+            'password' => bcrypt("guest@123"),
+            'role_id' => 4,
+            'jamath_id' => 1,
+        ]);
+        \App\Models\Roles::create([
+            'name' => 'SA',
+        ]);
+        \App\Models\Roles::create([
+            'name' => 'ADMIN',
+        ]);
+        \App\Models\Roles::create([
+            'name' => 'EDITOR',
+        ]);
+        \App\Models\Roles::create([
+            'name' => 'GUEST',
         ]);
     }
 }
