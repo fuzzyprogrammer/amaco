cff1d6c703b068cfa12cb355a67086ebab565c37
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-02-24 15:34:04 +0530

    updated

--- app/Models/DeliveryNoteDetail.php
+++ app/Models/DeliveryNoteDetail.php
@@ -76,7 +76,7 @@ class DeliveryNoteDetail extends Model
             // 'balance_quantity' => $this->getBalanceQuantity($totalQuantity, $totalDeliveredQuantity), //not required anymore
         ];
 
-        return ['delivery_notes_detail_'.$delivery_notes_detail->id => $data];
+        return [$data];
     }
 }
 
