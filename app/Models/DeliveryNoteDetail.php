63cfebba3a90924ca9891e8df40786fa4923deba
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-02-26 18:27:32 +0530

    updated

--- app/Models/DeliveryNoteDetail.php
+++ app/Models/DeliveryNoteDetail.php
@@ -66,7 +66,8 @@ class DeliveryNoteDetail extends Model
 
         $data = [
             "total_quantity" => $quotationDetail->quantity, //$totalQuantity =
-            "total_delivered_quantity" => $totalDeliveredQuantityExceptCurrentValue,
+            "total_delivered_quantity" => $totalDeliveredQuantity,
+            // "total_delivered_quantity" => $totalDeliveredQuantityExceptCurrentValue,
             "delivering_quantity" => $delivery_notes_detail->delivered_quantity,
             "delivery_notes_detail" => $delivery_notes_detail,
             "product" => array($delivery_notes_detail->product),
