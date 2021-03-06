95d81f1a337bfe6dddc259d3d904b2ecb6ff39d0
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-03-06 11:39:37 +0530

    updated

--- app/Http/Controllers/Api/ColumnController.php
+++ app/Http/Controllers/Api/ColumnController.php
@@ -32,16 +32,16 @@ class ColumnController extends Controller
         $data = $request->all();
         $accountCategoryId = $request->account_category_id;
         foreach ($data as $column ) {
-            if(!$column == $accountCategoryId){
-                Column::create(
-                    [
-                        'account_category_id' => $accountCategoryId,
-                        'name' => $column['name'],
-                        'type' => $column['type'],
-                    ]
-                );
+            if($column == $accountCategoryId){
+                continue;
             }
-
+            Column::create(
+                [
+                    'account_category_id' => $accountCategoryId,
+                    'name' => $column['name'],
+                    'type' => $column['type'],
+                ]
+            );
 
         }
 
