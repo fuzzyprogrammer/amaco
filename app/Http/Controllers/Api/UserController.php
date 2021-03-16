9b52a0daeedde3333efa0ea6189d0fcdb38cbc2b
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-03-16 16:47:50 +0530

    updated

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -134,7 +134,7 @@ class UserController extends Controller
 
     public function oldPassword(Request $request, User $user)
     {
-        if(Hash::check( $user->password , $request->password ) )
+        if(Hash::check($request->password, $user->password  ) )
         {
             return response()->json(true);
         }
