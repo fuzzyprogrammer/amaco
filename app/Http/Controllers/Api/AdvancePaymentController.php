9d41cf33b685470ab8ba23432fcc9f29351db249
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-03-20 14:33:50 +0530

    updated

--- app/Http/Controllers/Api/AdvancePaymentController.php
+++ app/Http/Controllers/Api/AdvancePaymentController.php
@@ -78,4 +78,20 @@ class AdvancePaymentController extends Controller
         return response()->json(['msg'=>"Successfully destroyed"], 200);
     }
 
+    public function accountStatement(Request $request)
+    {
+        $party = Party::find($request->party_id);
+        if (!$party) {
+            return response('No party exists by this id', 400);
+        }
+        $data = DB::table('parties')
+        ->join('invoices', 'invoices.party_id', '=', 'parties.id')
+        ->join('receipts', 'receipts.party_id', '=', 'parties.id')
+        ->where('id', $party->id)
+            ->whereBetween('created_at', [$request->from_date, $request->to_date])
+            ->orderBy('created_at')
+            ->get();
+
+        return response()->json($data);
+    }
 }
