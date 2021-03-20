4477fd26de551a495ea36ab7c33445dbef9d4339
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-03-20 17:01:46 +0530

    updated

--- app/Http/Controllers/Api/AccountStatementController.php
+++ app/Http/Controllers/Api/AccountStatementController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
 use App\Models\Party;
+use App\Models\Receipt;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -17,23 +20,34 @@ class AccountStatementController extends Controller
 
     public function accountStatement(Request $request)
     {
-        return response()->json($request);
+        // return response()->json($request);
         $party = Party::where('id' , intval($request['party_id']))->first();
         if(!$party){
             return response('No party exists by this id', 400);
         }
 
-        $data = DB::table('parties')
-            ->join('invoices','invoices.party_id','=','parties.id')
-            ->join('receipts', 'receipts.party_id','=','parties.id')
-            ->where('parties.id',$party->id)
-            ->whereBetween('invoices.created_at',[$request['from_date'], $request['to_date']])
-            ->orWhereBetween('receipts.created_at',[$request['from_date'], $request['to_date']])
-            ->select('parties.*',"parties.id as party_id", 'invoices.*','invoices.id as invoice_id', 'receipts.*', 'receipts.id as receipt_id')
-            ->orderBy('invoices.created_at')
-            ->orderBy('receipts.created_at')
+        // $data = DB::table('parties')
+        //     ->join('invoices','invoices.party_id','=','parties.id')
+        //     ->join('receipts', 'receipts.party_id','=','parties.id')
+        //     ->where('parties.id',$party->id)
+        //     ->orWhereBetween('receipts.created_at',[$request['from_date'], $request['to_date']])
+        //     ->select('parties.*',"parties.id as party_id", 'invoices.*','invoices.id as invoice_id', 'receipts.*', 'receipts.id as receipt_id')
+        //     ->orderBy('invoices.created_at')
+        //     ->orderBy('receipts.created_at')
+        //     ->get();
+        $invoiceCollection = new Collection();
+        $invoiceCollection = Invoice::where('party_id',$request['party_id'])
+            ->whereBetween('created_at',[$request['from_date'], $request['to_date']])
             ->get();
 
+        $receiptCollection = new Collection();
+        $receiptCollection = Receipt::where('party_id', $request['party_id'])
+            ->whereBetween('created_at',[$request['from_date'], $request['to_date']])
+            ->get();
+
+        foreach ($receiptCollection as $receipt) {
+            $data = $invoiceCollection->merge($receipt);
+        }
 
         return response()->json($data);
     }
