8d71d83a7171e6a8f356c799fc4f49bd75be42fb
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-03-20 11:27:40 +0530

    updated

--- database/migrations/2020_12_09_092514_create_r_f_q_s_table.php
+++ database/migrations/2020_12_09_092514_create_r_f_q_s_table.php
@@ -20,7 +20,6 @@ class CreateRFQSTable extends Migration
             $table->unsignedBigInteger('party_id')->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->unsignedBigInteger('contact_id')->nullable();
-            $table->text('files_url')->nullable();
             $table->timestamps();
         });
     }
