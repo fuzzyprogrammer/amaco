6a303fcfa376021f286225ac102b4723dc513553
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-03-22 11:37:31 +0530

    updated

--- app/Http/Controllers/Api/AccountStatementController.php
+++ app/Http/Controllers/Api/AccountStatementController.php
@@ -31,8 +31,8 @@ class AccountStatementController extends Controller
 
     public function accountStatement(Request $request)
     {
-        $res = $request->json()->all();
-        $party = Party::where('id', intval($res->party_id))->first();
+        
+        $party = Party::where('id', intval($request->party_id))->first();
         if (!$party) {
             return response('No party exists by this id', 400);
         }
