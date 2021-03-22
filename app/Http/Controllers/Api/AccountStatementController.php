514d9a85ce558565f2fb97087e0a7fc0d82053bb
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-03-22 14:41:14 +0530

    updated

--- app/Http/Controllers/Api/AccountStatementController.php
+++ app/Http/Controllers/Api/AccountStatementController.php
@@ -66,7 +66,7 @@ class AccountStatementController extends Controller
         $receiptCollection = $this->getReceiptData($party->id, $request['to_date'], $request['from_date']);
         $data = $invoiceCollection->merge($receiptCollection);
         $data = $data->sortBy('created_at');
-        $data && $data->each(function ($item) {
+        $data && $data->map(function ($item) {
             if($item->total_value){
                 $item['date'] = $item->created_at;
                 $item['code_no']= $item->invoice_no;
