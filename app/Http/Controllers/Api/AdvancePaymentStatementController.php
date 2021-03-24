9ddc6043625b4c61d2fe1caba4a6b9cb8d0cd039
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-03-24 11:59:22 +0530

    updated

--- app/Http/Controllers/Api/AdvancePaymentStatementController.php
+++ app/Http/Controllers/Api/AdvancePaymentStatementController.php
@@ -31,7 +31,8 @@ class AdvancePaymentStatementController extends Controller
 
     public function statement(Request $request)
     {
-        $paymentAccount = PaymentAccount::where('id', intval($request['payment_account_id']))->get();
+        // $paymentAccount = PaymentAccount::where('id', intval($request['payment_account_id']))->first();
+        $paymentAccount = PaymentAccount::find(intval($request['payment_account_id']));
         return $paymentAccount;
         if (!$paymentAccount) {
             return response('No paymentAccount exists by this id', 500);
