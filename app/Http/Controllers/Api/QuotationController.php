d4b4884d702b7003ef704a6076f39c0b2223c64a
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-03-30 12:04:30 +0530

    updated

--- app/Http/Controllers/Api/QuotationController.php
+++ app/Http/Controllers/Api/QuotationController.php
@@ -216,7 +216,7 @@ class QuotationController extends Controller
         try {
             $datas = [
                 'party_id' => $request['party_id'],
-                'rfq_id' => $request['rfq_id'] == null ? $request['rfq_id'] : null,
+                'rfq_id' => $request['rfq_id'] == null ? null : $request['rfq_id']  ,
                 'status' => 'New',
                 'total_value' => $request['total_value'],
                 'net_amount' => $request['net_amount'],
