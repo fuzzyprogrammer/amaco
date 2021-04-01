c53347e1fdcb3d25c26f75c78fdb90ec3eee1fd6
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-04-01 11:47:56 +0530

    updated

--- app/Http/Controllers/Api/TaxController.php
+++ app/Http/Controllers/Api/TaxController.php
@@ -24,6 +24,8 @@ class TaxController extends Controller
 
     public function purchaseTax(Request $request)
     {
+        return response()->json(now());
+
         if ($request->from_date) {
             $expenseCollection = Expense::whereBetween('created_at', [$request->from_date . ' ' . '00:00:00', $request->to_date ? $request->to_date . ' ' . '23:59:59' : now()])->get();
         } else {
