7bcfc2e089a87e15c5de15fb58d62dcbbbd13196
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-04-01 11:36:28 +0530

    updated

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -90,7 +90,7 @@ class ExpenseController extends Controller
             $tempFile = "file" . $column_data['id'];
             // return $tempFile;
             if ($request->file($tempFile)) {
-                $column_data_value = $request->file($tempFile)->move('expenses/files');
+                $column_data_value = $request->file($tempFile)->move('expenses/files/'. $request->file($tempFile)->getClientOriginalName());
             }
 
 
