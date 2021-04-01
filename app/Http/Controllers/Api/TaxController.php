589c9fc5f3f16c37bc7283ac7e8cd2c947bbd82b
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-04-01 11:34:35 +0530

    updated

--- app/Http/Controllers/Api/TaxController.php
+++ app/Http/Controllers/Api/TaxController.php
@@ -15,7 +15,7 @@ class TaxController extends Controller
         if ($request->from_date) {
             $invoiceCollection = Invoice::whereBetween('created_at', [$request->from_date . ' ' . '00:00:00', $request->to_date ? $request->to_date . ' ' . '23:59:59' : now()])->get();
         } else {
-            $invoiceCollection = Invoice::where('created_at', date('y')."-".date('m')."-"."01")->get();
+            $invoiceCollection = Invoice::where('created_at', date('Y-m-d'))->get();
         }
 
         return response()->json($invoiceCollection);
@@ -26,7 +26,7 @@ class TaxController extends Controller
         if ($request->from_date) {
             $expenseCollection = Expense::whereBetween('created_at', [$request->from_date . ' ' . '00:00:00', $request->to_date ? $request->to_date . ' ' . '23:59:59' : now()])->get();
         } else {
-            $expenseCollection = Expense::where('created_at', date('y') . "-" . date('m') . "-" . "01")->get();
+            $expenseCollection = Expense::where('created_at', date("Y-m-d"))->get();
         }
 
         return response()->json($expenseCollection);
