8ab3e1cbedab2c9eaecdf502ec0ad088bb85350e
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-04-21 12:58:58 +0530

    updated

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -75,7 +75,7 @@ class CompanyController extends Controller
      * @param  \App\Models\Company  $company
      * @return \Illuminate\Http\Response
      */
-    public function update(Company $company)
+    public function update( Request $request, Company $company)
     {
 
 
