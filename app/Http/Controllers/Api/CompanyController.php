f2eb071a3ed1b60d066c286c4b0931e31eda932f
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-04-21 12:00:49 +0530

    updated

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class CompanyController extends Controller
 {
@@ -76,18 +77,46 @@ class CompanyController extends Controller
      */
     public function update(Request $request, Company $company)
     {
+
+
+
+
         $data = $request->all();
         if ($request->file('img1')) {
+            if (File::exists(public_path($company->img1))) {
+
+                File::delete(public_path($company->img1));
+
+                $company->update([
+                    'img1' => null
+                ]);
+            }
             $img1_name = $request['img1']->getClientOriginalName();
             $img1_path = $request->file('img1')->move('company/', $img1_name);
             $data['img1'] = $img1_path;
         }
         if ($request->file('img2')) {
+            if (File::exists(public_path($company->img2))) {
+
+                File::delete(public_path($company->img2));
+
+                $company->update([
+                    'img2' => null
+                ]);
+            }
             $img2_name = $request['img2']->getClientOriginalName();
             $img2_path = $request->file('img2')->move('company/', $img2_name);
             $data['img2'] = $img2_path;
         }
         if ($request->file('img3')) {
+            if (File::exists(public_path($company->img3))) {
+
+                File::delete(public_path($company->img3));
+
+                $company->update([
+                    'img3' => null
+                ]);
+            }
             $img3_name = $request['img3']->getClientOriginalName();
             $img3_path = $request->file('img3')->move('company/', $img3_name);
             $data['img3'] = $img3_path;
