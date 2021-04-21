a84fd5ae95f46e94a9ca664ec916e9ed08f83c37
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-04-21 13:04:18 +0530

    updated

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Company;
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 
 class CompanyController extends Controller
