c4a95ed95efe62c8c374426325bba536819107df
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-04-21 12:48:00 +0530

    updated

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -80,7 +80,7 @@ class CompanyController extends Controller
 
 
 
-        $data = $request->all();
+        $data =(array)$request->all();
 
         if ($request->file('img1')) {
             if (File::exists(public_path($company->img1))) {
