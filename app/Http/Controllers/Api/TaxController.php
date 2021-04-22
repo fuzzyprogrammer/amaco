509df6d8925dad71400bf0e7c92532108845fe72
fuzzyprogrammer <fuzzyprogrammerincode@gmail.com>
2021-04-22 14:45:46 +0530

    updated

--- app/Http/Controllers/Api/TaxController.php
+++ app/Http/Controllers/Api/TaxController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Expense;
 use App\Models\Invoice;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class TaxController extends Controller
@@ -17,7 +16,7 @@ class TaxController extends Controller
         } else {
             $invoiceCollection = Invoice::whereBetween('created_at', [date("Y-m") . '-01' . ' ' . '00:00:00', $request->to_date ? $request->to_date . ' ' . '23:59:59' : now()])->get();
         }
-
+        $invoiceCollection->quotation->party;
         return response()->json($invoiceCollection);
     }
 
